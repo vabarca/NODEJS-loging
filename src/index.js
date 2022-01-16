f40a1478368194ede2cb03bc57c2acc58b6c3d14
V. Abarca <viabfer@gmail.com>
2022-01-16 20:34:45 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
+//1 - Invocamos a express
 const express = require('express');
-const engine = require('ejs-mate');
-const path = require('path');
-const morgan = require('morgan');
-
 const app = express();
 
-//settings
-app.set('views', path.join(__dirname, 'views'));
-app.engine('ejs', engine);
-app.set('view engine', 'ejs');
-app.set('port', process.env.PORT || 3000)
-
-//middlewares
-app.use(morgan('dev'));
+//2 - Seteamos urlencoded para capturar los datos del formulario
 app.use(express.urlencoded({extended:false}));
+app.use(express.json);
 
-//Routes
-app.use('/', require('./routes/routers'));
+//3 - Invocamos a dotenv
+const dotenv = require('dotenv');
+dotenv.config({path: './env/.env'});
 
-//stating server
-app.listen(app.get('port'), () => {
-    console.log('Server on Port', app.get('port'))
+//4 - Seteamos el directorio public
+app.use('/resources', express.static('public'));
+app.use('/resources', express.static(__dirname + '/public'));
+
+//5 - Establecemos el motor de pantallas ejs
+app.set('view engine', 'ejs');
+
+//6 - Invocamos a bcryptjs
+const bcryptjs = require('bcryptjs');
+
+//7 - Invocamos la sesion
+const session = require('express-session');
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+}));
+
+app.get('/', (req, res) => {
+    res.send('hola amegos!!');
+});
+
+app.listen(3000, (req, res) => {
+    console.log('Servidor running in localhost:3000');
 });
